c63e4321e25afac747a9001898813d343ebc7aa6
lou731 <marie.marnet.pro@gmail.com>
2017-01-23 23:13:42 +0100

    création classe département

--- Departement.php
+++ Departement.php
@@ -28,9 +28,96 @@ class Departement
 			// Alors pour chaque clé, on rÃ©cupÃ¨re sa valeur.
 			foreach($args as $key => $value)
 			{
-				// Si la propriété de la classe existe, alors on met à  jour sa valeur.
+				if($key == "contours") {
+                    if(is_array($value) && !empty($value))
+                    {
+                        $this->contours = $value;
+                    }
+                }
+
+                // Si la propriété de la classe existe, alors on met à  jour sa valeur.
 				if(isset($this->$key))	$this->$key = $value;
 			}
         }
+    }
+
+    /**
+     * Summary of set id
+     * @param int $id 
+     */
+    public function setId(int $id) {
+        $this->id = $id;
+    }
+
+    /**
+     * Summary of set nom
+     * @param string $nom 
+     */
+    public function setNom(string $nom) {
+        $this->nom = $nom;
+    }
+
+    /**
+     * Summary of set id region
+     * @param int $idRegion 
+     */
+    public function setIdRegion(int $idRegion) {
+        $this->id_region = $idRegion;
+    }
+
+    /**
+     * Summary of set Code
+     * @param int $code 
+     */
+    public function setCode(int $code) {
+        $this->code = $code;
+    }
+
+    /**
+     * Summary of set Contours
+     * @param array $contours 
+     */
+    public function setContours(array $contours) {
+        $this->contours = $contours;
+    }
+
+    /**
+     * Summary of get Id
+     * @return integer
+     */
+    public function getId() : int{
+        return $this->id;
+    }
+
+    /**
+     * Summary of get Nom
+     * @return integer
+     */
+    public function getNom() : string {
+        return $this->id;
+    }
+
+    /**
+     * Summary of get Id Region
+     * @return integer
+     */
+    public function getIdRegion() : int{
+        return $this->id_region;
+    }
+
+    /**
+     * Summary of get Code
+     * @return integer
+     */
+    public function getCode() : int{
+        return $this->code;
+    }
+
+    /**
+     * Summary of get Contours
+     * @return array
+     */
+    public function getContours() : array{
+        return $this->contours;
     }
 }
