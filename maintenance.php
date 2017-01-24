7562c23e3665dfc605daa82325cae6e944d8633a
Alex3132 <alexis.zueras.pro@gmail.com>
2017-01-24 12:56:09 +0100

    création de page + modif de log maintenance et recherche

--- maintenance.php
+++ maintenance.php
@@ -1,19 +1,25 @@
-<div id="recherche">
-    <label>Ville :</label>
-    <input type="text" id="ville">
-    <label>Département : </label>
-    <input type="text" id="dept">
-    <label>Régions : </label>
-    <input type="text" id="region">
-</div>
-<div id="resultat">
+<form action="#" method="post">
+    <div id="recherche">
+        <label>Ville :</label>
+        <input type="text" id="ville">
+        <label>Département : </label>
+        <input type="text" id="dept">
+        <label>Régions : </label>
+        <input type="text" id="region">
+    </div>
+    <div id="resultat">
 
 
 
-</div>
-<div id="modif">
+    </div>
+    <div id="modif">
 
 
 
 
-</div>
+    </div>
+
+
+    <input type="submit" value="Envoyer">
+
+</form>
--- recherche.php
+++ recherche.php
@@ -1,18 +1,26 @@
-<div id="recherche">
-    <div class="nom">
-        <label>Nom :</label>
-        <input type="text" id="nom">
-    </div>
-    <div class="dept">
-        <label>Département : </label>
-        <input type="text" id="dept">
-    </div>
-    <div class="region">
-        <label>Région : </label>
-        <input type="text" id="region">
+<form action="#" method="post">
+
+    <div id="recherche">
+        <div class="nom">
+            <label>Nom :</label>
+            <input type="text" id="nom">
+        </div>
+        <div class="dept">
+            <label>Département : </label>
+            <input type="text" id="dept">
+        </div>
+        <div class="region">
+            <label>Région : </label>
+            <input type="text" id="region">
+        </div>
+        <input type="submit" value="Envoyer">
     </div>
 
-</div>
+</form>
+
+
+
+
 
 <div id="resultat">
 
