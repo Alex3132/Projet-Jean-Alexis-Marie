8ca14d150f8270187fec7ea650ea7ffceb63910f
Alex3132 <alexis.zueras.pro@gmail.com>
2017-01-24 12:36:44 +0100

    et la page de maintenance

---
+++ maintenance.php
@@ -0,0 +1,19 @@
+<div id="recherche">
+    <label>Ville :</label>
+    <input type="text" id="ville">
+    <label>Département : </label>
+    <input type="text" id="dept">
+    <label>Régions : </label>
+    <input type="text" id="region">
+</div>
+<div id="resultat">
+
+
+
+</div>
+<div id="modif">
+
+
+
+
+</div>
