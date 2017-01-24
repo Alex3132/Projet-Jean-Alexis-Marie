f37f6057b96f99e69bb8f7c9393952346b678efc
Alex3132 <alexis.zueras.pro@gmail.com>
2017-01-24 12:35:54 +0100

    remplissage de ses feuilles php
    alors à regarder de preès car j'ai pu faire quelques erreurs

---
+++ login.php
@@ -0,0 +1,24 @@
+<div id="connection">
+
+    <div class="pseudo">
+        <label>Pseudo :</label>
+        <input type="text" id="pseudo">
+    </div>
+    <div class="password">
+        <label>Mot de passe :</label>
+        <input type="passeword" id="ville">
+    </div>
+
+</div>
+<div id="inscription">
+
+    <div class="pseudo">
+        <label>Pseudo :</label>
+        <input type="text" id="pseudo">
+    </div>
+    <div class="password">
+        <label>Mot de passe :</label>
+        <input type="passeword" id="ville">
+    </div>
+
+</div>
---
+++ recherche.php
@@ -0,0 +1,23 @@
+<div id="recherche">
+    <div class="nom">
+        <label>Nom :</label>
+        <input type="text" id="nom">
+    </div>
+    <div class="dept">
+        <label>Département : </label>
+        <input type="text" id="dept">
+    </div>
+    <div class="region">
+        <label>Région : </label>
+        <input type="text" id="region">
+    </div>
+
+</div>
+
+<div id="resultat">
+
+    <div class="local">
+    </div>
+
+
+</div>
