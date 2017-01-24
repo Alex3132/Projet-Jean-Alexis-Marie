149d40d7027b844a4aa281c4aeb92aa8ba75ab27
Alex3132 <alexis.zueras.pro@gmail.com>
2017-01-24 14:19:34 +0100

    ajout de la page inscription.php

---
+++ inscription.php
@@ -0,0 +1,15 @@
+<form action="inscrit.php" method="post">
+    <div id="inscription">
+
+        <div class="pseudo">
+            <label>Pseudo :</label>
+            <input type="text" id="pseudo">
+        </div>
+        <div class="password">
+            <label>Mot de passe :</label>
+            <input type="passeword" id="ville">
+        </div>
+        <input type="submit" value="Envoyer">
+
+    </div>
+</form>
--- login.php
+++ login.php
@@ -15,19 +15,3 @@
 
 
 </form>
-
-<form action="inscrit.php" method="post">
-    <div id="inscription">
-
-        <div class="pseudo">
-            <label>Pseudo :</label>
-            <input type="text" id="pseudo">
-        </div>
-        <div class="password">
-            <label>Mot de passe :</label>
-            <input type="passeword" id="ville">
-        </div>
-        <input type="submit" value="Envoyer">
-
-    </div>
-</form>
