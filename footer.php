982b0118ed00a8848c8125d47c3cc916fcb7e0ae
jean.mariette <jean.mariette.pro@gmail.com>
2017-01-27 14:16:41 +0100

    doc
    en petit... très petit

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>Fait par Marie, Jean, Alexis copyright 2017</p>
+    <p>Fait par Marie, Jean, Alexis copyright 2017 / <span style="font-size : 0.5em;">Doc : </span><a style="font-size : 0.5em;" href="doc/Projet%20MongoDB.html">par ici.</a></p> 
 </footer>
