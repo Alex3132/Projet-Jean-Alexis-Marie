16dca9c06db0135dd2e1ad0bbac7b3de2499c690
lou731 <marie.marnet.pro@gmail.com>
2017-01-27 10:55:15 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	recherche.php

--- recherche.php
+++ recherche.php
@@ -68,9 +68,9 @@ EOSVGH;
     $lat_min = 90;
     foreach($dep->getContours() as $coord)
     {
-        if ($coord->lat < $lon_max) $lat_max = $coord->lat;
-        if ($coord->lon < $lon_max) $lon_max = $coord->lon;
-        if ($coord->lat > $lat_min) $lat_min = $coord->lat;
+        if ($coord->lat > $lat_max) $lat_max = $coord->lat;
+        if ($coord->lon > $lon_max) $lon_max = $coord->lon;
+        if ($coord->lat < $lat_min) $lat_min = $coord->lat;
         if ($coord->lon < $lon_min) $lon_min = $coord->lon;
     }
 
@@ -88,137 +88,6 @@ EOSVGH;
     //$command = new MongoDB\Driver\Command(['find' => 'contours']); // ces lignes correspondens à l'utilisation de 'find'
     //$curseur = $mgc->executeCommand($dbname, $command);
 
-    //foreach($dep->getContours() as $coord) {
-    //    echo '<polygon style="fill:#CCF" points="'."\n";
-    //    //$contour = $doc->poly;
-    //    //foreach ($contour as $ptb) {
-    //    list($px, $py) = projection($coord->lon, $coord->lat);
-    //    printf(' %d %d', $px, $py);
-    //}
-
-    //echo '"/>'."\n";
-
-    //// dessin des contours des départements qui peuvent être multiples (îles, enclaves)
-    //// Il est plus habile de placer d'abord les départements à contour simple puis ceux à contours doubles, puis triples...
-    //// afin de ne pas masquer les enclaves telles que celles qui apparaissent entre les Pyrénées-Atlantiques et les Hautes-Pyrénées.
-    //// Pour s'en rendre compte il suffit d'inverser l'ordre de sortie en changeant le signe de 'contours' dans l'appel de 'sort'
-    //// ce qui aura pour effet de retourner l'ordre de tri fixé ici en ordre croissant et dépendant du nombre de polygones par contour
-    //// et par conséquent d'augmenter la probabilité de faire disparaître certaines enclaves.
-
-    //// commande de récupération de tous les contours de l'ensemble de départements
-
-    //$filter = ['contours' => ['$exists' => true]]; // on veut que le département présente bien un contour
-    //$options = [
-    //  'projection' => ['contours' => 1, '_id_region' => 1, '_id' => 0],  // on ne veut que ces colonnes sans l'identifiant
-    //  'sort'       => ['contours' => 1],                                 // triées selon le nombre de contours ordre croissant
-    //];
-
-    //$query = new MongoDB\Driver\Query($filter, $options);
-    //$curseur = $mgc->executeQuery($dbname.'.departements', $query);
-
-    //foreach($curseur as $crs) {   // boucle sur l'ensemble des résultats
-    //    // si la région existe on prend la couleur attribuée sinon on génère une couleur de région aléatoire.
-    //    $idr = $crs->_id_region;
-    //    if (empty($colr[$idr])) {
-    //        $colr[$idr] = [rand(5,14), rand(5,14), rand(5,14)];
-    //    }
-    //    // la couleur du département a une composante régionale forte + une composante départementale faible et aléatoire
-    //    $color = sprintf('#%1x%1x%1x', rand(0,1)+$colr[$idr][0], rand(0,1)+$colr[$idr][1], rand(0,1)+$colr[$idr][2]);
-    //    foreach ($crs->contours as $contour) {
-    //        printf('<polygon fill="%s" points="'."\n", $color);
-    //        foreach ($contour as $ptb) {
-    //            list($lon, $lat) = $ptb;
-    //            list($px, $py) = projection($lon, $lat);
-    //            printf(' %d %d', $px, $py);
-    //        }
-    //        echo '"/>'."\n";
-    //    }
-    //}
-
-    //// affichage les noms des régions centrés dans l'espace occupé par chacune d'elles
-    //// utilisation de la technique map/reduce pour récupérer le rectangle englobant chaque région
-    //// création de la fonction de mapping
-    //$map_funct = <<<'EOMAP'
-    //function() {
-    //    var res = {
-    //      minlon: 180,
-    //      maxlon: -180,
-    //      minlat: 90,
-    //      maxlat: -90
-    //    }
-    //    for (i=0; i<this.contours.length; i++) {
-    //      for (j=0; j<this.contours[i].length; j++) {
-    //        if (res.minlon > this.contours[i][j][0]) res.minlon = this.contours[i][j][0];
-    //        if (res.maxlon < this.contours[i][j][0]) res.maxlon = this.contours[i][j][0];
-    //        if (res.minlat > this.contours[i][j][1]) res.minlat = this.contours[i][j][1];
-    //        if (res.maxlat < this.contours[i][j][1]) res.maxlat = this.contours[i][j][1];
-    //      }
-    //    }
-    //    emit(1, res);
-    //  }
-    //EOMAP;
-
-    //// création de la fonction de réduction
-    //$reduce_funct = <<<'EORED'
-    //function(key, vals) {
-    //  var res = {
-    //    minlon: 180,
-    //    maxlon: -180,
-    //    minlat: 90,
-    //    maxlat: -90
-    //  }
-    //  vals.forEach(function(val) {
-    //    if (res.minlon > val.minlon) res.minlon = val.minlon;
-    //    if (res.maxlon < val.maxlon) res.maxlon = val.maxlon;
-    //    if (res.minlat > val.minlat) res.minlat = val.minlat;
-    //    if (res.maxlat < val.maxlat) res.maxlat = val.maxlat;
-    //  });
-    //  return {minmax: res};
-    //}
-    //EORED;
-
-    // boucle sur les régions
-    // astuce pour éviter d'utiliser la commande 'find' non suportée avant mongodb 3.2
-    //$curseur = $mgc->executeQuery($dbname.'.regions', new MongoDB\Driver\Query(['_id' => ['$gt' => 0]]));
-    ////$curseur = $mgc->executeCommand($dbname, new MongoDB\Driver\Command(['find' => 'regions']));
-    //foreach($curseur as $doc) {
-    //    // préparation de la requête utilisant les fonctions précédemment définies sur la région courante.
-    //    $command = new MongoDB\Driver\Command([
-    //      'mapreduce' => 'departements',
-    //      'map' => $map_funct,
-    //      'reduce' => $reduce_funct,
-    //      'query' => ['contours' => ['$exists' => true], '_id_region' => $doc->_id],
-    //      'out' => ['inline' => 1],
-    //    ]);
-    //    $cbox = $mgc->executeCommand($dbname, $command)->toArray()[0];
-    //    if (isset($cbox->results[0])) {
-    //        $box = $cbox->results[0]->value->minmax;
-    //        $lat = ($box->maxlat + $box->minlat)/2;
-    //        $lon = ($box->maxlon + $box->minlon)/2;
-    //        list($px, $py) = projection($lon, $lat);
-    //        printf('<g class="regions"><text x="%d" y="%d">%s</text>'."\n", $px, $py, $doc->nom);
-    //        printf('<circle cx="%d" cy="%d" r="15"/></g>'."\n", $px, $py);
-    //    }
-    //    //print_r($cbox);exit;
-
-    //    //// récupération du résultat utile
-    //    //// var box = res.results[0].value.minmax;
-    //}
-
-    // affichage des villes les plus importantes de chaque département mais de population < 200000 habitants
-    // commande de récupération des coordonnées, population et nom par agrégation
-    //$command = new MongoDB\Driver\Command(['aggregate' => 'villes',                                    // le choix de l'opération, ici 'aggregate'
-    //                       'pipeline'=> [['$sort' => ['_id_dept' => 1, 'pop' => 1]],   // on trie sur ces deux colonnes
-    //                             ['$group' => ['_id' => '$_id_dept',           // groupement par départements
-    //                                   'pop' => ['$last' => '$pop'],   // on ne retient que la dernière population
-    //                                   'nom' => ['$last' => '$nom'],   //                   le dernier nom
-    //                                   'lon' => ['$last' => '$lon'],   //                   la dernière longitude
-    //                                   'lat' => ['$last' => '$lat']]], //                et la dernière latitude
-    //                             ['$match' => ['pop' => ['$lt' => $min_pop_cities]]], // on élimine les villes >= 200000 habitants
-    //                       ]]);
-    //$cities = $mgc->executeCommand($dbname, $command)->toArray()[0];
-
-    //foreach($curseur as $crs) {   // boucle sur l'ensemble des résultats
         // si la région existe on prend la couleur attribuée sinon on génère une couleur de région aléatoire.
 
         // la couleur du département a une composante régionale forte + une composante départementale faible et aléatoire
@@ -244,24 +113,6 @@ EOSVGH;
     list($px, $py) = projection($ville->lon, $ville->lat);
     printf('<g><text x="%d" y="%d">%s</text>'."\n", $px, $py, $ville->nom);
     printf('<circle cx="%d" cy="%d" r="5"/></g>'."\n", $px, $py);
-
-    //foreach($cities->result as $doc) {
-    //    list($px, $py) = projection($doc->lon, $doc->lat);
-    //    printf('<g  class="cities"><text x="%d" y="%d">%s</text>'."\n", $px, $py, $doc->nom);
-    //    printf('<circle cx="%d" cy="%d" r="5"/></g>'."\n", $px, $py);
-    //}
-
-    //// affichage de toutes les villes de population >= 200000 habitants
-    //$filter = ['pop' => ['$gte' => $min_pop_cities]];
-    //$options = ['projection' => ['nom' => 1, 'lon' => 1, 'lat' => 1, '_id' => 0]];  // on ne veut que ces colonnes sans l'identifiant
-    //$query = new MongoDB\Driver\Query($filter, $options);
-    //$curseur = $mgc->executeQuery($dbname.'.villes', $query);
-
-    //foreach($curseur as $doc) {
-    //    list($px, $py) = projection($doc->lon, $doc->lat);
-    //    printf('<g class="cities"><text x="%d" y="%d">%s</text>'."\n", $px, $py, $doc->nom);
-    //    printf('<circle cx="%d" cy="%d" r="4"/></g>'."\n", $px, $py);
-    //}
 
     echo $svgfooter;
 }
@@ -300,12 +151,10 @@ function showResultVille($villerep, $connect, $connecte) {
 
     echo "</div>";
     echo "</form>";
-
-    showCarte($ville, $dept);
 }
 
 function showCarte(Ville $ville, Departement $dep) {
-    echo "<div>";
+    echo "<div id='cdep' class='gridrech'>";
     drawCarteDep($ville, $dep);
     echo "</div>";
 }
@@ -375,23 +224,25 @@ function showChoiceVille($villerep, $connect) {
         </form>
     </div>
 
-    <?php 
-    
+    <?php
+
     $titleserach = "";
     if(isset($ville)) {
         $titleserach = $ville->getNom();
+        $dep = $connect->findDepById($ville->_id_dept);
     }
     elseif (isset($_POST['choixville'])) {
         $choixville=$_POST['choixville'];
         $villerep2 = $connect->findVilleById($choixville);
+        $ville = $villerep2;
+        $dep = $connect->findDepById($villerep2->_id_dept);
         $titleserach = $villerep2->getNom();
-    } 
+    }
     else if(isset($_POST['nom'])) {
         $titleserach = $_POST['nom'];
     }
 
     ?>
-
     <div id="resultat" class='containerform gridrech'>
         <div class='titreform'>Résultat de la recherche pour <?php echo  $titleserach ?></div>
         <!--<form action='#' method='post'>
@@ -435,23 +286,15 @@ function showChoiceVille($villerep, $connect) {
         //$choixville=$_POST['choixville'];
         //$villerep2 = $connect->FindVilleByNomChoosen($choixville);
         showResultVille($villerep2, $connect, $connecte);
-        //$dept= $villerep2[0]->_id_dept;
-        //$findept= $connect->findDepById($dept);
-        //echo "<div><input type=\"text\" name=\"valeur[]\" value=\"dep=>".$findept->getNom()."\" hidden readonly>Departement : ".$findept->getNom()."</div>\n";
-        //$findregion = $connect->findRegionById($findept->getIdregion());
-        //echo "<div class=\"region\"><input type=\"text\" name=\"valeur[]\" value=\"reg=>".$findregion->getNom()."\" hidden readonly>Region : ".$findregion->getNom()."</div>\n";
-        //echo "<div class=\"nom\"><input type=\"text\" name=\"valeur[]\" value=\"nom=>".$villerep2[0]->nom."\" hidden readonly>Nom : ".$villerep2[0]->nom."</div>\n";
-        //echo "<input type=\"text\" name=\"valeur[]\" value=\"_id=>".$villerep2[0]->_id."\" hidden readonly>";
-        //if(property_exists($villerep2[0], 'pop') && $villerep2[0]->pop)
-        //    echo "<div class=\"pop\"><input type=\"text\" name=\"valeur[]\" value=\"pop=>".$villerep2[0]->pop."\" hidden readonly>Pop : ".$villerep2[0]->pop."</div>\n";
-        //if($villerep2[0]->lat)
-        //    echo "<div class=\"lat\"><input type=\"text\" name=\"valeur[]\" value=\"lat=>".$villerep2[0]->lat."\" hidden readonly>Lat : ".$villerep2[0]->lat."</div>\n";
-        //if($villerep2[0]->lon)
-        //    echo "<div class=\"lon\"><input type=\"text\" name=\"valeur[]\" value=\"lon=>".$villerep2[0]->lon."\" hidden readonly>Lon : ".$villerep2[0]->lon."</div>\n";
-        //if($villerep2[0]->cp)
-        //    echo "<div class=\"cp\"><input type=\"text\" name=\"valeur[]\" value=\"cp=>".$villerep2[0]->cp."\" hidden readonly>Code Postal : ".$villerep2[0]->cp."</div>\n";
     }
         ?>
     </div>
+    </div>
+
+    <div>
+        <?php 
+    if(isset($ville) && isset($dep))
+        showCarte($ville, $dep);
+    ?>
     </div>
 
